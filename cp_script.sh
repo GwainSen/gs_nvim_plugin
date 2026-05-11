6fa8e5726520d00f8aecd61d46edb84accaa3820
GwainSen <gwainsen@gmail.com>
2026-05-11 18:40:38 +0200

    added basic lsp functionality

--- cp_script.sh
+++ cp_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo cp -r ../gs_nvim_plugin /usr/local/share/lua/5.1
+sudo cp -r ../gs_nvim_plugin ~/.config/nvim/lua/
---
+++ remap.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = " "
+--usure if needed maybe restructure
+--maybe change the keys
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
